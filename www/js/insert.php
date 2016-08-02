6d9db288cdaea656e7503189fd7eb686f037ee10
michaelreda <michaelreda96@gmail.com>
2016-08-02 19:30:20 +0200

    anyhost

--- www/js/insert.php
+++ www/js/insert.php
@@ -10,9 +10,9 @@ $faculty = mysql_real_escape_string($data->faculty);
 $dash = mysql_real_escape_string($data->dash);
 
 
-$host="michaelreda.github.io";
+$host="%";//anyhost
 
-mysql_connect($host,"github","123456") or die(mysql_error());
+mysql_connect($host,"any","") or die(mysql_error());
 mysql_select_db("pyf") or die(mysql_error());
 mysql_query("INSERT INTO members(first_name,last_name,age,home_city,mobile_number,church,faculty,class)VALUES('".$first_name."','".$last_name."','".$age."','".$homeCity."','".$mobile."','".$church."','".$faculty."','".$dash."')");
 $id = json_decode(mysql_insert_id());
