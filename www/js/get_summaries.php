8e7a1acc394026d4fd4f1a0a5839e0ffc59406f9
michaelreda <michaelreda96@gmail.com>
2016-08-03 10:27:30 +0200

    Update get_summaries.php

--- www/js/get_summaries.php
+++ www/js/get_summaries.php
@@ -1,8 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-
-$conn = new mysqli("localhost", "root", "", "pyf");
+$host="127.7.30.2";
+//mysql_connect($host,"adminWgLJxMp","H18sCM-e3FKn") or die(mysql_error());
+$conn = new mysqli($host,"adminWgLJxMp","H18sCM-e3FKn", "pyf");
 
 $result = $conn->query("SELECT id, title, date FROM summaries");
 
