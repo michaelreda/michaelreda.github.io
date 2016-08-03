175989d8b576942cb4e48df4e8f01c430e671d9b
michaelreda <michaelreda96@gmail.com>
2016-08-03 10:25:07 +0200

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -118,6 +118,11 @@ angular.module('app.controllers', ['ionic'])
           //  $('#side-menu21').hide();
             // Triggered on a button click, or some other target
             $scope.Join = function (first_name, last_name, age, homeCity, mobile, church, faculty, dash) {
+                 $http.get("js/get_summaries.php")
+                    .then(function (response) {
+                        $scope.summaries = response.data.records;
+                    });
+                
                 
                 if (first_name == undefined || last_name == undefined || age == undefined || homeCity == undefined || mobile == undefined || church == undefined || faculty == undefined || dash == undefined) {
                     var alertPopup = $ionicPopup.alert({
